fd51c895f9ca9191e5ea71295eb4b02700930c95
daniel <danielortega.shz@gmail.com>
2021-11-19 16:07:15 -0600

    try a new decorator

--- decorator.py
+++ decorator.py
@@ -14,3 +14,13 @@ def greetings():
 greetings()
 
 
+def Uppercase(function):
+    def nested(text):
+       return function(text).upper()
+    return nested
+
+@Uppercase
+def message(name):
+    return f'{name}, you have a message'
+
+print(message('Daniel'))
